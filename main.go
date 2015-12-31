7eb1b88c6d87f94364c9bd0192ceb091016530aa
charles <charles.d.burton@gmail.com>
2015-12-31 16:05:35 -0700

    Changed the command line parser to use the codegangsta library for command line options.  It's much nicer than the built-in one.

--- main.go
+++ main.go
@@ -2,39 +2,67 @@ package main
 
 import (
 	"log"
-	"flag"
 	"golem/master"
 	"os"
 	"os/signal"	
 	"syscall"
+	"github.com/codegangsta/cli"
+)
+
+var (
+	mode = "cli"
+	commPort = 10000
+	dataPort = 10001
+	bindIP = "0.0.0.0"
 )
 
 func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go handleCtrlC(c)
-	var (
-		mode = flag.String("mode", "cli", "The operational mode of the server")
-		commPort = flag.Int("commport", 10000, "The listening communication port")
-		dataPort = flag.Int("dataport", 10001, "The port for data transfers")
-		bindIP = flag.String("bind", "0.0.0.0", "The IP you want the server to bind to")		
-	)
+	golem := cli.NewApp()
+	golem.Name = "golem"
+	golem.Author = "Charles Burton"
+	golem.Email = "charles.d.burton@gmail.com"
+	golem.Usage = "A fast remote execution engine written in Go"
+	golem.Flags = []cli.Flag {
+		cli.StringFlag {
+			Name: "m, mode",
+			Value: "cli",
+			Usage: "Set the application mode to peon, master, or overlord.  Defaults to command line",
+			Destination: &mode,
+		},
+		cli.IntFlag {
+			Name: "d, dataport",
+			Value: 10001,
+			Usage: "Set the server port that data will transfer over. Default: 10001",
+			Destination: &dataPort,
+		},
+		cli.IntFlag {
+			Name: "c, commport",
+			Value: 10000,
+			Usage: "Set the port that golem will communicate with. Default: 10000",
+			Destination: &commPort,
+		},
+		cli.StringFlag {
+			Name: "b, bind",
+			Value: "0.0.0.0",
+			Usage: "Sets the IP golem will bind to.",
+			Destination: &bindIP,
+		},
+	}
 	
-	flag.Parse()
+	golem.Commands = []cli.Command {
+		
+	}
+	golem.Run(os.Args)
 	
-	if *mode == "master" {
-		log.Println("master")
-		master.StartMaster(*commPort, *dataPort, *bindIP, "master")
-	} else if *mode == "overlord" {
-		log.Println("overlord")
-	} else if *mode == "peon" {
-		log.Println("peon")
-	} else if *mode == "cli" {
-		log.Println(*mode)
-	} else {
-		log.Println("Invalid mode setting")
+	if mode == "master" {
+		master.StartMaster(commPort, dataPort, bindIP, mode)
 	}
 }
+
+
 
 func handleCtrlC(c chan os.Signal) {
 	sig := <-c
