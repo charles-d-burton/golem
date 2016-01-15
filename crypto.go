e54487c7370b28d543de6097a016c4823148d255
charles <charles.d.burton@gmail.com>
2016-01-15 15:28:39 -0700

    Added file for crypto helpers.  This will ensure that all connections are established with crytography.

---
+++ crypto.go
@@ -0,0 +1,37 @@
+package crypto
+import (
+	"os"
+)
+
+/*
+Functions in this package help with generating and loading keys for use by
+the overlord, master, and peon.  They are used to secure communication
+*/
+
+// MakeSSHKeyPair make a pair of public and private keys for SSH access.
+// Public key is encoded in the format for inclusion in an OpenSSH authorized_keys file.
+// Private Key generated is PEM encoded
+func MakeSSHKeyPair(pubKeyPath, privateKeyPath string) error {
+    privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+    if err != nil {
+        return err
+    }
+
+    // generate and write private key as PEM
+    privateKeyFile, err := os.Create(privateKeyPath)
+    defer privateKeyFile.Close()
+    if err != nil {
+        return err
+    }
+    privateKeyPEM := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)}
+    if err := pem.Encode(privateKeyFile, privateKeyPEM); err != nil {
+        return err
+    }
+
+    // generate and write public key
+    pub, err := ssh.NewPublicKey(&privateKey.PublicKey)
+    if err != nil {
+        return err
+    }
+    return ioutil.WriteFile(pubKeyPath, ssh.MarshalAuthorizedKey(pub), 0655)
+}
