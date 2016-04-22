1759a8766992c2d2d09b771068be454533d3e026
Charles D. Burton <charles.burton@markwhatney.fsl.noaa.gov>
2016-04-22 18:42:34 +0000

    Added stub for mesh.go

--- net/bus/mesh.go
+++ net/bus/mesh.go
@@ -1,3 +1,11 @@
 package bus
 
-func 
+/*
+StartMesh ... 
+This is intended to start the mesh networking piece.
+Ideally this will enable the CM system to be self-healing and
+provide resiliency against host and network failure.
+*/
+func StartMesh() {
+    //TODO: Write the code to actually start and build the mesh network
+}
